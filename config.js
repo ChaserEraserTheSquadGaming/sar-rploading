a44db1410f8a41b7ea5b197b3e06358a3eace6d3
ChaserEraserTheSquadGaming <33849826+ChaserEraserTheSquadGaming@users.noreply.github.com>
2021-05-17 12:29:05 -0400

    Add files via upload

---
+++ config.js
@@ -0,0 +1,17 @@
+/* Need Help? Join my discord @ discord.gg/yWddFpQ */
+var config = {
+    /* In milliseconds */
+    "transitionInterval": 1000,
+    "imgInterval": 3250,
+    "music": false,
+    /* Insert the Youtube Video ID below */
+    "videoID": "jo4-FhqkNwQ",
+    /* Music Volume (0%-100%) | Lower is Quieter, Higher is Louder */
+    "musicVolume": 70,
+    "text": {
+        "title": "San Andreas Realism Roleplay",
+        "link": "Real People, Real Scenarios",
+    },
+    /* You dont have to include the '/imgs/' dir AND ALSO REMEMBER TO ADD THESE IMGS IN YOUR __resource.lua*/ 
+    "images": ['bg0.png', 'bg1.png', 'bg2.png', 'bg3.png', 'bg4.png', 'bg5.png', 'bg6.png', 'bg7.png', 'bg8.png', 'bg9.png', 'bg10.png', 'bg11.png', 'bg12.png', 'bg13.png','bg14.png', 'bg15.png', 'bg16.png', 'bg17.png', 'bg18.png', 'bg19.png', 'bg20.png', 'bg21.png', 'bg22.png', 'bg23.png', 'bg24.png', 'bg25.png', 'bg25.png', 'bg26.png', 'bg27.png', 'bg28.png', 'bg29.png', 'bg30.png', 'bg31.png', 'bg32.png', 'bg33.png',]
+}
